7f1ef004a3dd718207efa23e2b614835e78a500b
Maya Messinger <maya.messinger@duke.edu>
2018-04-29 16:07:02 -0400

    Found problem with public path - round 2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: path.resolve(__dirname, './dist'),
+    publicPath: '/final-project-team-44/dist/',
     filename: 'build.js'
   },
   module: {
